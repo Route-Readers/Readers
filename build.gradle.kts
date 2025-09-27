f1fdfe688670378a5cfde5d9d8777d43f4d030c8
hyunwoo00 <lukert@gachon.ac.kr>
2025-09-27 22:03:50 +0900

    복구...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false        // Compose 컴파일러 플러그인 추가 (Kotlin 버전과 일치시키세요)
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
 }
