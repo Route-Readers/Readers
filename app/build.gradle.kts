8f5079067fb763b845768f66bffcf79101df7687
isaac8570 <isaac8570@hanmail.net>
2025-11-07 12:21:51 +0900

    add: 앱 시작 스플레시 화면에 리더스 글씨 추가 - 어쨌든 보이긴 함 .. 아이콘 배율과 위치 수정 필요, 오류 안남

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {id("com.android.application")
 
 android {
     namespace = "com.route.readers"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.route.readers"
@@ -54,6 +54,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.13.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
@@ -67,7 +68,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.core:core-splashscreen:1.2.0")
 
     implementation("androidx.compose.foundation:foundation-layout:1.6.8")
 
@@ -78,7 +79,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-messaging")
-    implementation("com.google.accompanist:accompanist-permissions:0.34.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
