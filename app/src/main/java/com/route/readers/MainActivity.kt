dabc05c8dbb61dae1d74266dd760d498e8404a6a
hyunwoo00 <lukert@gachon.ac.kr>
2025-11-12 20:18:53 +0900

    위젯화면수정

--- app/src/main/java/com/route/readers/MainActivity.kt
+++ app/src/main/java/com/route/readers/MainActivity.kt
@@ -66,6 +66,7 @@ import com.route.readers.ui.screens.profile.ProfileUiState
 import com.route.readers.ui.screens.profile.ProfileViewModel
 import com.route.readers.ui.screens.profile.StatisticsScreen
 import com.route.readers.ui.screens.token.TokenShopScreen
+import com.route.readers.ui.screens.widget.WidgetScreen
 import com.route.readers.ui.theme.ReadersTheme
 import com.route.readers.widget.WidgetUpdateHelper
 import java.net.URLDecoder
@@ -471,6 +472,12 @@ fun RootAppNavigation(accountViewModel: AccountViewModel) {
         composable("goal_route") {
             GoalScreen(
                 onBack = { appNavController.popBackStack() }
+            )
+        }
+
+        composable("widget_route") {
+            WidgetScreen(
+                onNavigateBack = { appNavController.popBackStack() }
             )
         }
 
