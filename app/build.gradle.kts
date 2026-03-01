c10871a496f1a9c526fc0f2c78425d0b838306b0
hyunwoo00 <lukert@gachon.ac.kr>
2026-03-01 20:37:23 +0900

    알림 오류해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.route.readers"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 101
+        versionName = "1.0.11"
 
         val localProperties = Properties()
         val localPropertiesFile = rootProject.file("local.properties")
